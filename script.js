7aa49536540f0cc9a9788aa23a24afaf21fe1f71
riggschrom3act <riggschrom3act@gmail.com>
2025-11-09 22:14:45 -0600

    adding basic resume as functional websit tested locally

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scrolling for nav links
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        const target = document.querySelector(link.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
