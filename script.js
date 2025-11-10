c7d0e86f3e106cc2a7e11c664e69c15cd4f25e44
riggschrom3act <riggschrom3act@gmail.com>
2025-11-09 22:37:44 -0600

    adding as one single file instead for easy of use and to showcase better handling and understanding of basic code

--- script.js
+++
@@ -1,8 +0,0 @@
-// Smooth scrolling for nav links
-document.querySelectorAll('nav ul li a').forEach(link => {
-    link.addEventListener('click', e => {
-        e.preventDefault();
-        const target = document.querySelector(link.getAttribute('href'));
-        target.scrollIntoView({ behavior: 'smooth' });
-    });
-});
